9987073dcb1d07dae0a5c875fbb1b581c47d505b
Raniere Prates <rsilva@uplexis.com.br>
2021-01-22 17:23:48 -0300

    criei mocks que testam se no teste foi chamado o método save

--- parte_2/src/FidelityProgramBundle/Test/Service/FidelityProgramServiceTest.php
+++ parte_2/src/FidelityProgramBundle/Test/Service/FidelityProgramServiceTest.php
@@ -16,6 +16,45 @@ class FidelityProgramServiceTest extends TestCase
      */
     public function shouldSaveWhenReceivePoints()
     {
+        $pointsRepository = $this->createMock(PointsRepository::class);
 
+        $pointsRepository->expects($this->once())->method('save');
+
+        $pointsCalculator = $this->createMock(PointsCalculator::class);
+        $loggerInterface = $this->createMock(LoggerInterface::class);
+
+        $pointsCalculator->method('calculatePointsToReceive')
+            ->willReturn(100);
+
+        $fidelityProgramService = new FidelityProgramService($pointsRepository, $pointsCalculator, $loggerInterface);
+
+        $customer = $this->createMock(Customer::class);
+        $value = 50;
+
+        $fidelityProgramService->addPoints($customer, $value);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotSaveWhenNotReceivePoints()
+    {
+        $pointsRepository = $this->createMock(PointsRepository::class);
+
+        $pointsRepository->expects($this->never())
+            ->method('save');
+
+        $pointsCalculator = $this->createMock(PointsCalculator::class);
+        $loggerInterface = $this->createMock(LoggerInterface::class);
+
+        $pointsCalculator->method('calculatePointsToReceive')
+            ->willReturn(0);
+
+        $fidelityProgramService = new FidelityProgramService($pointsRepository, $pointsCalculator, $loggerInterface);
+
+        $customer = $this->createMock(Customer::class);
+        $value = 20;
+
+        $fidelityProgramService->addPoints($customer, $value);
     }
 }
