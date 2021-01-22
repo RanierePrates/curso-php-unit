5bdc48490769eaf1fb7ca865bdbb2a52a1fde4e3
Raniere Prates <rsilva@uplexis.com.br>
2021-01-22 17:08:12 -0300

    deletei os arquivos para fazer acompanhando a aula

--- parte_2/src/FidelityProgramBundle/Test/Service/FidelityProgramServiceTest.php
+++ parte_2/src/FidelityProgramBundle/Test/Service/FidelityProgramServiceTest.php
@@ -16,63 +16,6 @@ class FidelityProgramServiceTest extends TestCase
      */
     public function shouldSaveWhenReceivePoints()
     {
-        $pointsRepository = $this->createMock(PointsRepository::class);
-        $pointsRepository->expects($this->once())
-            ->method('save');
 
-        $pointsCalculator = $this->createMock(PointsCalculator::class);
-        $pointsCalculator->method('calculatePointsToReceive')
-            ->willReturn(100);
-
-        $allMessages = [];
-        $logger = $this->createMock(LoggerInterface::class);
-        $logger->method('log')
-            ->will($this->returnCallback(
-                function ($message) use (&$allMessages) {
-                    $allMessages[] = $message;
-                }
-            ));
-
-        $fidelityProgramService = new FidelityProgramService(
-            $pointsRepository,
-            $pointsCalculator,
-            $logger
-        );
-
-        $customer = $this->createMock(Customer::class);
-        $value = 50;
-        $fidelityProgramService->addPoints($customer, $value);
-
-        $expectedMessages = [
-            'Checking points for customer',
-            'Customer received points'
-        ];
-        $this->assertEquals($expectedMessages, $allMessages);
-    }
-
-    /**
-     * @test
-     */
-    public function shouldNotSaveWhenReceiveZeroPoints()
-    {
-        $pointsRepository = $this->createMock(PointsRepository::class);
-        $pointsRepository->expects($this->never())
-            ->method('save');
-
-        $pointsCalculator = $this->createMock(PointsCalculator::class);
-        $pointsCalculator->method('calculatePointsToReceive')
-            ->willReturn(0);
-
-        $logger = $this->createMock(LoggerInterface::class);
-
-        $fidelityProgramService = new FidelityProgramService(
-            $pointsRepository,
-            $pointsCalculator,
-            $logger
-        );
-
-        $customer = $this->createMock(Customer::class);
-        $value = 20;
-        $fidelityProgramService->addPoints($customer, $value);
     }
 }
